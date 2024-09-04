97c41cdd2895ffc341ac75884dbad5e805e92f48
AndriBryn <andri@mtsport.is>
2024-09-04 09:52:15 +0000

    laga functioninn meira

--- netlify/functions/update.cjs
+++ netlify/functions/update.cjs
@@ -1,16 +1,15 @@
+const { Octokit } = require('@octokit/core')
+
 exports.handler = async function (event, context) {
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN // Use environment variables for the token
+  const octokit = new Octokit({ auth: GITHUB_TOKEN })
+
+  const owner = 'AndriBryn' // Replace with your GitHub username or organization
+  const repo = 'website' // Replace with your repository name
+  const path = 'public/data/clubs.csv' // Path to the file in the repo
+  const branch = 'main' // Branch to commit to
+
   try {
-    // Dynamically import the Octokit core package as it is an ES module
-    const { Octokit } = await import('@octokit/core')
-
-    const GITHUB_TOKEN = process.env.GITHUB_TOKEN // Use environment variables for the token
-    const octokit = new Octokit({ auth: GITHUB_TOKEN })
-
-    const owner = 'AndriBryn' // Replace with your GitHub username or organization
-    const repo = 'website' // Replace with your repository name
-    const path = 'public/data/clubs.csv' // Path to the file in the repo
-    const branch = 'main' // Branch to commit to
-
     const { csvContent } = JSON.parse(event.body)
 
     // Get the latest commit SHA and the tree SHA it points to
