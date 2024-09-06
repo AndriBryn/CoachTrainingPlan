a2583422246ce0d940e492cbd9a523a2e8cd1c3f
AndriBryn <andri@mtsport.is>
2024-09-06 11:40:33 +0000

    testa útlit

--- src/App.vue
+++ src/App.vue
@@ -110,6 +110,7 @@
                     justify-content: space-between;
                     text-align: center;
                     border: solid;
+                    transform: translateY(0%);
                   "
                 >
                   <input type="checkbox" v-model="club.exercises" :value="exercise.exercise" />
